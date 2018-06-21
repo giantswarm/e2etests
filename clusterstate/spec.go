f76cabe858981df7259c805f38503cbdb954021e
Ross Fairbanks <ross@giantswarm.io>
2018-06-21 18:06:52 +0200

    Address review comments

--- clusterstate/spec.go
+++ clusterstate/spec.go
@@ -4,6 +4,14 @@ import (
 	"context"
 )
 
+const (
+	CNRAddress      = "https://quay.io"
+	CNROrganization = "giantswarm"
+	ChartChannel    = "stable"
+	ChartName       = "e2e-app-chart"
+	ChartNamespace  = "e2e-app"
+)
+
 type Interface interface {
 	// Test executes the cluster state test using the configured provider
 	// implementation. The provider implementation has to be aware of the guest
@@ -12,13 +20,15 @@ type Interface interface {
 	// master node.
 	//
 	//  - Install test app.
-	//  - Wait for cluster to be ready.
+	//  - Check test app is installed.
 	//  - Reboot master node.
 	//  - Wait for API to be down.
 	//  - Wait for cluster to be ready.
+	//  - Check test app is installed.
 	//  - Replace master node.
 	//  - Wait for API to be down.
 	//  - Wait for cluster to be ready.
+	//  - Check test app is installed.
 	//
 	Test(ctx context.Context) error
 }
