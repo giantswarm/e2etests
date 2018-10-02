54f260dad999b8acaa90474e740285dea65a5e3d
Oliver Ponder <oliver.ponder@gmail.com>
2018-10-02 21:47:58 +0800

    Try to fix broken e2e tests.

--- vendor/github.com/giantswarm/e2e-harness/pkg/framework/resource/resource.go
+++ vendor/github.com/giantswarm/e2e-harness/pkg/framework/resource/resource.go
@@ -1,6 +1,7 @@
 package resource
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -11,8 +12,6 @@ import (
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/afero"
 	"k8s.io/helm/pkg/helm"
-
-	"github.com/giantswarm/e2e-harness/pkg/framework"
 )
 
 const (
@@ -78,9 +77,32 @@ func New(config Config) (*Resource, error) {
 
 func (r *Resource) Delete(name string) error {
 	err := r.helmClient.DeleteRelease(name, helm.DeletePurge(true))
-	if err != nil {
+	if helmclient.IsReleaseNotFound(err) {
+		return microerror.Maskf(releaseNotFoundError, name)
+	} else if helmclient.IsTillerNotFound(err) {
+		return microerror.Mask(tillerNotFoundError)
+	} else if err != nil {
 		return microerror.Mask(err)
 	}
+
+	return nil
+}
+
+func (r *Resource) EnsureDeleted(ctx context.Context, name string) error {
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ensuring deletion of release %#q", name))
+
+	err := r.helmClient.DeleteRelease(name, helm.DeletePurge(true))
+	if helmclient.IsReleaseNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q does not exist", name))
+	} else if helmclient.IsTillerNotFound(err) {
+		r.logger.LogCtx(ctx, "level", "warning", "message", "tiller is not found/installed")
+	} else if err != nil {
+		return microerror.Mask(err)
+	} else {
+		r.logger.LogCtx(ctx, "level", "info", "message", fmt.Sprintf("deleted release %#q", name))
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("ensured deletion of release %#q", name))
 
 	return nil
 }
@@ -98,7 +120,7 @@ func (r *Resource) Install(name, values, channel string, conditions ...func() er
 	}
 
 	for _, c := range conditions {
-		err = backoff.Retry(c, backoff.NewExponential(framework.ShortMaxWait, framework.ShortMaxInterval))
+		err = backoff.Retry(c, backoff.NewExponential(backoff.ShortMaxWait, backoff.ShortMaxInterval))
 		if err != nil {
 			return microerror.Mask(err)
 		}
@@ -142,7 +164,7 @@ func (r *Resource) WaitForStatus(release string, status string) error {
 		r.logger.Log("level", "debug", "message", fmt.Sprintf("failed to get release status '%s': retrying in %s", status, t), "stack", fmt.Sprintf("%v", err))
 	}
 
-	b := backoff.NewExponential(framework.ShortMaxWait, framework.LongMaxInterval)
+	b := backoff.NewExponential(backoff.MediumMaxWait, backoff.LongMaxInterval)
 	err := backoff.RetryNotify(operation, b, notify)
 	if err != nil {
 		return microerror.Mask(err)
@@ -166,7 +188,7 @@ func (r *Resource) WaitForVersion(release string, version string) error {
 		r.logger.Log("level", "debug", "message", fmt.Sprintf("failed to get release version '%s': retrying in %s", version, t), "stack", fmt.Sprintf("%v", err))
 	}
 
-	b := backoff.NewExponential(framework.ShortMaxWait, framework.LongMaxInterval)
+	b := backoff.NewExponential(backoff.ShortMaxWait, backoff.LongMaxInterval)
 	err := backoff.RetryNotify(operation, b, notify)
 	if err != nil {
 		return microerror.Mask(err)
